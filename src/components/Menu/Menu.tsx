c0d8db921093d02b440196e0922fcb8892ea0a52
Luis Peraza <leperazas@gmail.com>
2025-05-19 13:18:46 +0200

    Agregados los botones

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,19 +1,44 @@
+import styles from "./Menu.module.css";
+
 export default function Menu() {
     return (
-        <>
-            <div>
-                <span> LOGO </span>
-                <span> Nombre </span>
-            </div>
-            <div>
-                Lista de menus
-            </div>
-            <div>
-                Selector de Idiomas
-            </div>
-            <div>
-                Imagenes de Redes Sociales
-            </div>
-        </>
+        <div className={styles["header"]}>
+            <header className={styles["header-container"]}>
+                <div className={styles["header-logo-container"]}>
+                    <span> LOGO </span>
+                    <span> Nombre </span>
+                </div>
+                <div className={styles["header-menu-container"]}>
+                    <nav>
+                        <a href="">
+                            <span>
+                                About me
+                            </span>
+                        </a>
+                        <a href="">
+                            <span>
+                                Technologies
+                            </span>
+                        </a>
+                        <a href="">
+                            <span>
+                                Porfolio
+                            </span>
+                        </a>
+                        <a href="">
+                            <span>
+                                Contact
+                            </span>
+                        </a>
+                    </nav>
+                    <div>
+                        Selector de Idiomas
+                    </div>
+                    <div>
+                        Imagenes de Redes Sociales
+                    </div>
+                </div>
+            </header>
+        </div>
     )
 }
